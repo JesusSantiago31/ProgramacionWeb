84aa4179eae5ff38ae8aa52184bd3a75760fb465
JesusSantiago31 <jesussantiagocr31@gmail.com>
2024-11-24 10:30:00 -0600

    Task 3

--- Courses/Coursera/Introduccion al Desarrollo del Front-End/Getting Started/JavaScript/OOP/BuildingAOrientedObjectParadigm/ooprogramming.js
+++ Courses/Coursera/Introduccion al Desarrollo del Front-End/Getting Started/JavaScript/OOP/BuildingAOrientedObjectParadigm/ooprogramming.js
@@ -1,32 +1,35 @@
 // Task 1: Code a Person class
 class Person{
     constructor(name = "Tom", age = 20, energy = 100){
-        this.name = name
-        this.age = age
-        this.energy = energy
+        this.name = name;
+        this.age = age;
+        this.energy = energy;
     }
 
     sleep(energy){
-        energy += 10
+        energy += 10;
         }
     doSomethingFun(energy){
-        energy -= 10
+        energy -= 10;
     }
 }
 // Task 2: Code a Worker class
 class Worker extends Person{
     constructor(xp = 0, hourlyWage = 10, name, age, anaergy){
-        this.xp = xp
-        this.hourlyWage = 10
+        this.xp = xp;
+        this.hourlyWage = 10;
     }
     goToWork(xp){
-        xp += 10
+        xp += 10;
     }
 }
 
 // Task 3: Code an intern object, run methods
 function intern() {
+    var intern = new Worker("Bob", 21, 110, 0, 10);
 
+    intern.goToWork();
+    return intern;
 }
 
 // Task 4: Code a manager object, methods
