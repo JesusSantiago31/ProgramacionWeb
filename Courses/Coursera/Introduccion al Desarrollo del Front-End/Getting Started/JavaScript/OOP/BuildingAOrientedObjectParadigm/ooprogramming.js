67d575fba0998f1475c65eceff40907aafb3cdc2
JesusSantiago31 <jesussantiagocr31@gmail.com>
2024-11-24 11:30:00 -0600

    Task 4

--- Courses/Coursera/Introduccion al Desarrollo del Front-End/Getting Started/JavaScript/OOP/BuildingAOrientedObjectParadigm/ooprogramming.js
+++ Courses/Coursera/Introduccion al Desarrollo del Front-End/Getting Started/JavaScript/OOP/BuildingAOrientedObjectParadigm/ooprogramming.js
@@ -34,5 +34,7 @@ function intern() {
 
 // Task 4: Code a manager object, methods
 function manager() {
-    
+    var manager = new Worker("Alice", 30, 120, 100, 30)
+    manager.doSomethingFun
+    return manager;
 }
