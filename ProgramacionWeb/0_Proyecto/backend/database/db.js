eea4fe1c9ca1454045180037ea751df26c5fa2ae
JesusSantiago31 <jesussantiagocr31@gmail.com>
2025-02-02 05:07:00 -0600

    DataBase Simulation

---
+++ ProgramacionWeb/0_Proyecto/backend/database/db.js
@@ -0,0 +1,18 @@
+const productos = [{
+    id:1,
+    nombre:"galletas",
+    precio:500,
+    stock: 12
+},
+{
+    id:2,
+    nombre:"chocolates",
+    precio:10,
+    stock: 100
+}]; // Arreglo vacío
+
+/* Forma explicíta
+modules.exports = {
+    productos:productos
+}*/
+module.exports={productos};
