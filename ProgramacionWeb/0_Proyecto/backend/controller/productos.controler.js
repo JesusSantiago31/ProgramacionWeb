68559f04087daa00c04d85df4bec5d8df9a0fd3a
JesusSantiago31 <jesussantiagocr31@gmail.com>
2025-02-17 05:07:00 -0600

    Estructura Update y Delete

--- ProgramacionWeb/0_Proyecto/backend/controller/productos.controler.js
+++ ProgramacionWeb/0_Proyecto/backend/controller/productos.controler.js
@@ -17,7 +17,29 @@ class ProductoController{
         return res.json(ProductosService.create(nombre,precio,stock));
     }
 
+    static update(req,res){
+        const {nombre, precio, stock}=req.body;
+
+        return res.json
+    }
     // TODO: Colocar el controller del update y delete
+
+    update(id, objeto) {
+        const index = productos.findIndex((pt) => pt.id == id);
+        if (index === -1) return null;
+        
+        productos[index] = { ...productos[index], ...objeto };
+        return productos[index];
+    }
+
+    delete(id) {
+        const index = productos.findIndex((pt) => pt.id == id);
+        if (index === -1) return null;
+        
+        const productoEliminado = productos.splice(index, 1);
+        return productoEliminado[0];
+    }
+
 }
 
 module.exports = {ProductoController}
