813d6230d28422e6be73c24ed1b0a18d75b3b386
JesusSantiago31 <jesussantiagocr31@gmail.com>
2025-02-20 05:07:00 -0600

    Script Correccion Errores

--- Practicas/1_Cronometro/script/script.js
+++ Practicas/1_Cronometro/script/script.js
@@ -2,12 +2,15 @@ const stopwatch = document.getElementById('stopwatch');
 const playPauseButton = document.getElementById('play-pause');
 const secondsSphere = document.getElementById('seconds-sphere')
 
-let stopWatchInterval;
-let runningTime
+let stopwatchInterval;
+let runningTime = 0;
 
 const playPause = () =>{
     const isPaused = !playPauseButton.classList.contains('running');
     if(isPaused){
+        playPauseButton.classList.add('running');
+        start();
+    }else {
         playPauseButton.classList.remove('running');
         pause();
     }
@@ -15,7 +18,7 @@ const playPause = () =>{
 
 const pause = () =>{
     secondsSphere.style.animationPlayState = 'paused'
-    clearInterval(stopWatchInterval);
+    clearInterval(stopwatchInterval);
 }
 
 const stop = () =>{
@@ -23,16 +26,16 @@ const stop = () =>{
     secondsSphere.style.animation = 'none';
     playPauseButton.classList.remove('running');
     runningTime = 0;
-    clearInterval(stopWatchInterval);
-    stopwatch.textContent = "00:00";
+    clearInterval(stopwatchInterval);
+    stopwatch.textContent = '00:00';
 }
 
 const start = () =>{
     secondsSphere.style.animation = 'rotacion 60s linear infinite';
     let startTime = Date.now() - runningTime;
     secondsSphere.style.animationPlayState = 'running';
-    stopWatchInterval = setInterval( () => {
-        runningTime = Date.now() .startTime;
+    stopwatchInterval = setInterval( () => {
+        runningTime = Date.now() - startTime;
         stopwatch.textContent=calculateTime(runningTime);
     }, 1000);
 }
@@ -41,8 +44,8 @@ const calculateTime = runningTime => {
     const total_seconds = Math.floor(runningTime / 1000);
     const total_minutes = Math.floor(total_seconds / 60);
 
-    const display_seconds = (total_secon % 60).toString().padStart(2, "0")
-    const display_minutes = total_minutes.toString().padStart(1, "0")
+    const display_seconds = (total_seconds % 60).toString().padStart(2, "0")
+    const display_minutes = total_minutes.toString().padStart(2, "0")
 
-    return '${display_minutes]:${display_seconds}'
+    return `${display_minutes}:${display_seconds}`
 }
