8015694ed8c12cdac2cce8ed376d90ec73992c39
JesusSantiago31 <jesussantiagocr31@gmail.com>
2025-04-03 10:13:00 -0600

    .\TaskItem.jsx

---
+++ ProgramacionWeb/4_proyecto2_react/src/components/TaskItem.jsx
@@ -0,0 +1,7 @@
+const TaskItem = ({task}) =>{
+    return(
+        <li className="border-b border-cyan-200 ps-4 font-xl hover:scale-103 bg-white duration-300">{task}</li>
+    )
+}
+
+export default TaskItem;
