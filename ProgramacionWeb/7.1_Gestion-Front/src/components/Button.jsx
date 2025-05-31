5157eb4fdf2667f4b2a7522e6bb637d0df9a6dee
JesusSantiago31 <jesussantiagocr31@gmail.com>
2025-05-31 10:15:00 -0600

    Aprendiendo REACT

---
+++ ProgramacionWeb/7.1_Gestion-Front/src/components/Button.jsx
@@ -0,0 +1,13 @@
+
+
+const Button = ({text}) => {
+
+
+  return (
+    <button
+    className="w-full bg-emerald-400 rounded py-2 hover:bg-emerald-600 cursor-pointer">
+      {text}</button>
+  )
+}
+
+export default Button
